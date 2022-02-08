3523990b8652d87ebd4c94b6545b1690d978ff83
madumakk <madumakeletso123@gmail.com>
2022-02-08 22:21:29 +0200

    2nd commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 10){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
